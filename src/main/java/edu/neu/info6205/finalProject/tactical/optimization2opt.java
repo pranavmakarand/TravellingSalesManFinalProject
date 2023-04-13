e6271ec2d5adc650769678ddd10a4cce0d28793b
pranavmakarand <pranavmakarand94@gmail.com>
2023-04-13 10:12:40 -0400

    2opt3opt

---
+++ src/main/java/edu/neu/info6205/finalProject/tactical/optimization2opt.java
@@ -0,0 +1,30 @@
+package edu.neu.info6205.finalProject.tactical;
+import java.util.Collections;
+import java.util.List;
+
+import edu.neu.csye6205.finalProject.Paurush.Node;
+import edu.neu.csye6205.finalProject.Paurush.util.Distance;
+public class optimization2opt {
+	public static List<Node> twoOpt(List<Node> tour) {
+	    int size = tour.size();
+	    boolean improved = true;
+	    while (improved) {
+	        improved = false;
+	        for (int i = 0; i < size - 2; i++) {
+	            for (int j = i + 2; j < size - 1; j++) {
+	                double dist1 = Distance.measureDistance(tour.get(i), tour.get(i + 1));
+	                double dist2 = Distance.measureDistance(tour.get(j), tour.get(j + 1));
+	                double dist3 = Distance.measureDistance(tour.get(i), tour.get(j));
+	                double dist4 = Distance.measureDistance(tour.get(i + 1), tour.get(j + 1));
+	                if (dist1 + dist2 > dist3 + dist4) {
+	                    Collections.reverse(tour.subList(i + 1, j + 1));
+	                    improved = true;
+	                }
+	            }
+	        }
+	    }
+	    return tour;
+		 
+//	   
+	}
+}
---
+++ src/main/java/edu/neu/info6205/finalProject/tactical/optimization3opt.java
@@ -0,0 +1,55 @@
+package edu.neu.info6205.finalProject.tactical;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.neu.csye6205.finalProject.Paurush.Node;
+import edu.neu.csye6205.finalProject.Paurush.util.Distance;
+
+public class optimization3opt {
+	public static List<Node> threeOpt(List<Node> route) {
+	    boolean improved = true;
+	    while (improved) {
+	        improved = false;
+	        for (int i = 0; i < route.size() - 5; i++) {
+	            for (int j = i + 2; j < route.size() - 3; j++) {
+	                for (int k = j + 2; k < route.size() - 1; k++) {
+	                    List<Node> newRoute = new ArrayList<Node>();
+	                    // First segment of the route remains the same
+	                    for (int x = 0; x <= i; x++) {
+	                        newRoute.add(route.get(x));
+	                    }
+	                    // 2-opt swap
+	                    for (int x = j; x >= i + 1; x--) {
+	                        newRoute.add(route.get(x));
+	                    }
+	                    // Second segment of the route remains the same
+	                    for (int x = k; x >= j + 1; x--) {
+	                        newRoute.add(route.get(x));
+	                    }
+	                    // 2-opt swap
+	                    for (int x = k + 1; x < route.size(); x++) {
+	                        newRoute.add(route.get(x));
+	                    }
+	                    // Calculate the distance of the new route
+	                    double newDistance = calculateDistance(newRoute);
+	                    // If the new route is shorter, replace the old route with the new one
+	                    if (newDistance < calculateDistance(route)) {
+	                        route = newRoute;
+	                        improved = true;
+	                    }
+	                }
+	            }
+	        }
+	    }
+	    return route;
+	}
+	public static double calculateDistance(List<Node> route) {
+	    double distance = 0;
+	    for (int i = 0; i < route.size() - 1; i++) {
+	        distance += Distance.measureDistance(route.get(i), route.get(i + 1));
+	    }
+	    return distance;
+	}
+
+}
